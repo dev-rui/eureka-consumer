ad3f5eb931247c98b041690c31582317d52bfdff
heruihong0325 <18655377896@163.com>
2019-12-17 15:44:03 +0800

    新增断路器和rabbitmq监听

--- src/main/java/com/example/eurekaconsumer/controller/DcController.java
+++ src/main/java/com/example/eurekaconsumer/controller/DcController.java
@@ -1,8 +1,10 @@
 package com.example.eurekaconsumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,15 +17,39 @@ public class DcController {
 
    /* @Autowired
     LoadBalancerClient loadBalancerClient;*/
+    /*@Autowired
+    RestTemplate restTemplate;*/
+
     @Autowired
-    RestTemplate restTemplate;
+    ConsumerService consumerService;
 
     @GetMapping("/consumer")
     public String dc() {
        /* ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
         System.out.println(url);
-        return restTemplate.getForObject(url, String.class);*/
-        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);*/
+        return consumerService.consumer();
     }
+    @Service
+    class ConsumerService {
+
+        @Autowired
+        RestTemplate restTemplate;
+
+        @HystrixCommand(fallbackMethod = "fallback")
+        public String consumer() {
+            return restTemplate.getForObject("http://eureka-client/dc", String.class);
+        }
+
+        public String fallback() {
+            return "fallback";
+        }
+
+    }
+
+
+
+
 }
